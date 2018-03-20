ceaafb24eeb48735fe082f3dce78604a2a1603bc
naitik0212 <shahnaitik0212@gmail.com>
2018-03-19 20:07:12 -0700

    Initial commit

---
+++ src/main/java/com/cloud/initialserver/InitialserverApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.initialserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialserverApplication.class, args);
+	}
+}
