7547eddfc23848b14379382213d1f478c450f311
Sharvil Shah <sharvil@sharvil.local>
2018-03-27 12:56:29 -0700

    added cache

---
+++ src/main/java/com/cloud/initialserver/AmazonClient.java
@@ -0,0 +1,51 @@
+package com.cloud.initialserver;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@Service
+public class AmazonClient {
+
+    private AmazonS3 s3client;
+
+    @Value("${amazonProperties.endpointUrl}")
+    private String endpointUrl;
+
+    @Value("${amazonProperties.bucketName}")
+    private String bucketName;
+
+    @Value("${amazonProperties.accessKey}")
+    private String accessKey;
+
+    @Value("${amazonProperties.secretKey}")
+    private String secretKey;
+
+        @Value("${amazonProperties.region}")
+        private String awsRegion;
+
+        @PostConstruct
+        private void initializeAmazon() {
+            AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+            this.s3client = new AmazonS3Client(credentials);
+            this.s3client.setRegion(Region.getRegion(Regions.fromName(awsRegion)));
+        }
+
+        public void uploadFileTos3bucket(String key, String value) {
+            byte[] bytes = value.getBytes();
+            InputStream valueContent = new ByteArrayInputStream(bytes);
+            s3client.putObject(new PutObjectRequest(bucketName, key, valueContent, null)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        }
+}
--- src/main/java/com/cloud/initialserver/RequestController.java
+++ src/main/java/com/cloud/initialserver/RequestController.java
@@ -4,24 +4,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Map;
+
 @RestController
 @RequestMapping(value = "/cloudimagerecognition", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "*")
 public class RequestController {
 
+    private final AmazonClient amazonClient;
+
     private final Producer producer;
 
+    private static Hashtable<String, String> cache = new Hashtable<>();
+
+    private static Hashtable<String, Integer> imageFrequencyMap = new Hashtable<>();
+
+    private static final Integer CACHE_CAPACITY = 20;
+
     @Autowired
-    public RequestController(Producer producer) {
+    public RequestController(AmazonClient amazonClient, Producer producer) {
+        this.amazonClient = amazonClient;
         this.producer = producer;
     }
-
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public String receiveUserURL(@RequestParam(value = "input") String imageUrl) {
         if (imageUrl == null) {
             return "Image URL is required";
+        }
+        String[] imageUrlArr = imageUrl.split("/");
+        String imageName = imageUrlArr[imageUrlArr.length-1];
+        if (cache.containsKey(imageName)) {
+            String identifiedImage = cache.get(imageName);
+            amazonClient.uploadFileTos3bucket(imageName, identifiedImage);
+            return identifiedImage;
         }
         String id = String.valueOf(generateNumber());
         String sqsMessage = id + "__" + imageUrl;
@@ -31,6 +50,7 @@ public class RequestController {
             if(Consumer.finalTable.containsKey(id)) {
                 String result = Consumer.finalTable.get(id);
                 Consumer.finalTable.remove(id);
+                putToCache(imageName, result);
                 return result;
             }
         }
@@ -38,5 +58,38 @@ public class RequestController {
 
     private long generateNumber() {
         return (long)(Math.random()*100000 + 3333300000L);
+    }
+
+    private void putToCache(String imageName, String identifiedImage) {
+        if (cache.size() > CACHE_CAPACITY) {
+            String leastUsedKey = getLeastUsedKey();
+            if (leastUsedKey != null) {
+                cache.remove(leastUsedKey);
+                imageFrequencyMap.remove(leastUsedKey);
+            } else {
+                Map.Entry<String, String> entry = cache.entrySet().iterator().next();
+                cache.remove(entry.getKey());
+                imageFrequencyMap.remove(entry.getKey());
+            }
+        }
+        putToMap(imageName, identifiedImage);
+    }
+
+    private void putToMap(String imageName, String identifiedImage) {
+        cache.put(imageName, identifiedImage);
+        if (imageFrequencyMap.contains(imageName)) {
+            imageFrequencyMap.put(imageName, imageFrequencyMap.get(imageName) + 1);
+        } else {
+            imageFrequencyMap.put(imageName, 1);
+        }
+    }
+
+    private String getLeastUsedKey() {
+        for (String key: imageFrequencyMap.keySet()) {
+            if (imageFrequencyMap.get(key).equals(Collections.min(imageFrequencyMap.values()))) {
+                return key;
+            }
+        }
+        return null;
     }
 }
