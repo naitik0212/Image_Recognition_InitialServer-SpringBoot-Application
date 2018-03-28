9543d0013deb1a1f79ea3864c6818d10dec240d6
naitik0212 <shahnaitik0212@gmail.com>
2018-03-27 17:01:54 -0700

    removed caching

--- src/main/java/com/cloud/initialserver/RequestController.java
+++ src/main/java/com/cloud/initialserver/RequestController.java
@@ -13,19 +13,10 @@ import java.util.Map;
 @CrossOrigin(origins = "*")
 public class RequestController {
 
-    private final AmazonClient amazonClient;
-
     private final Producer producer;
 
-    private static Hashtable<String, String> cache = new Hashtable<>();
-
-    private static Hashtable<String, Integer> imageFrequencyMap = new Hashtable<>();
-
-    private static final Integer CACHE_CAPACITY = 20;
-
     @Autowired
-    public RequestController(AmazonClient amazonClient, Producer producer) {
-        this.amazonClient = amazonClient;
+    public RequestController(Producer producer) {
         this.producer = producer;
     }
 
@@ -35,13 +26,6 @@ public class RequestController {
         if (imageUrl == null) {
             return "Image URL is required";
         }
-        String[] imageUrlArr = imageUrl.split("/");
-        String imageName = imageUrlArr[imageUrlArr.length-1];
-        if (cache.containsKey(imageName)) {
-            String identifiedImage = cache.get(imageName);
-            amazonClient.uploadFileTos3bucket(imageName, identifiedImage);
-            return identifiedImage;
-        }
         String id = String.valueOf(generateNumber());
         String sqsMessage = id + "__" + imageUrl;
         producer.sendMessages(sqsMessage);
@@ -50,7 +34,6 @@ public class RequestController {
             if(Consumer.finalTable.containsKey(id)) {
                 String result = Consumer.finalTable.get(id);
                 Consumer.finalTable.remove(id);
-                putToCache(imageName, result);
                 return result;
             }
         }
@@ -58,38 +41,5 @@ public class RequestController {
 
     private long generateNumber() {
         return (long)(Math.random()*100000 + 3333300000L);
-    }
-
-    private void putToCache(String imageName, String identifiedImage) {
-        if (cache.size() > CACHE_CAPACITY) {
-            String leastUsedKey = getLeastUsedKey();
-            if (leastUsedKey != null) {
-                cache.remove(leastUsedKey);
-                imageFrequencyMap.remove(leastUsedKey);
-            } else {
-                Map.Entry<String, String> entry = cache.entrySet().iterator().next();
-                cache.remove(entry.getKey());
-                imageFrequencyMap.remove(entry.getKey());
-            }
-        }
-        putToMap(imageName, identifiedImage);
-    }
-
-    private void putToMap(String imageName, String identifiedImage) {
-        cache.put(imageName, identifiedImage);
-        if (imageFrequencyMap.contains(imageName)) {
-            imageFrequencyMap.put(imageName, imageFrequencyMap.get(imageName) + 1);
-        } else {
-            imageFrequencyMap.put(imageName, 1);
-        }
-    }
-
-    private String getLeastUsedKey() {
-        for (String key: imageFrequencyMap.keySet()) {
-            if (imageFrequencyMap.get(key).equals(Collections.min(imageFrequencyMap.values()))) {
-                return key;
-            }
-        }
-        return null;
     }
 }
